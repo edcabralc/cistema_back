7b776589f7eb9decb9f72a0fd3edcac77006f543
Edvaldo Cabral <cabralnet@gmail.com>
2024-04-03 11:15:29 -0300

    feature: initial server

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+import routes from "./routes/routes";
+
+dotenv.config();
+
+const server = express();
+
+server.use(express.static(path.join(__dirname, "../public")));
+server.use(express.urlencoded({ extended: true }));
+
+server.use(routes);
+server.use((req, res) => res.status(404).send("Página não econtrada"));
+
+server.listen(process.env.PORT);
