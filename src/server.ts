dd4111ffd0d1b6450c60d024f2c2c8447dbe67fa
Edvaldo Cabral <cabralnet@gmail.com>
2024-04-03 16:05:20 -0300

    fix: change name server

--- src/server.ts
+++ src/server.ts
@@ -7,16 +7,16 @@ import routes from "./routes/routes";
 
 dotenv.config();
 
-const server = express();
+const app = express();
 
-server.use(express.static(path.join(__dirname, "../public")));
-server.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.urlencoded({ extended: true }));
 
-server.use(routes);
-server.use((req, res) => res.status(404).send("Página não econtrada"));
+app.use(routes);
+app.use((req, res) => res.status(404).send("Página não econtrada"));
 
 connectDB().then(() => {
-  server.listen(process.env.PORT, () => {
+  app.listen(process.env.PORT, () => {
     console.log(`Listen to port: ${process.env.PORT}`);
   });
 });
